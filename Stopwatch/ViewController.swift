7d03c14f23a1414dbe2f4a8799ddf4dbb1b1964f
ambrr <ambermorris@Ambers-MacBook-Air.local>
2015-07-24 16:56:03 -0400

    initial commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Amber Morris on 2015-07-24.
+//  Copyright (c) 2015 Amber Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    var count = 0
+    
+    func updateTime() {
+        
+        count++
+        
+        time.text = "\(count)"
+        
+    }
+    
+    
+    
+    @IBOutlet weak var time: UILabel!
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+        count = 0
+        
+        time.text = "0"
+        
+    }
+    
+    
+    @IBAction func start(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
